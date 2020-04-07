c7b0d498c24b908ee76979f33b54767643fccb82
Thomas <62804686+0xThxmxs@users.noreply.github.com>
2020-04-07 17:34:26 +0200

    Add files via upload

---
+++ BotSample.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import DiscordPyMD.DiscordPyMD as dpymd
+
+
+bot = commands.Bot(command_prefix=".")
+
+
+@bot.event
+async def on_ready():
+	print("{bot.ready_msg}")
+
+
+@bot.command()
+async def dpymd_try(ctx):
+	await ctx.send(dpymd.bold("Some bold text."))
+	await ctx.send(dpymd.code("Some code."))
+	await ctx.send(dpymd.code_line("Some code single line."))
+	await ctx.send(dpymd.italics("Some italics text."))
+	await ctx.send(dpymd.strikethrough("Some strikethrough text."))
+	await ctx.send(dpymd.underline("Some underlined text."))
+	await ctx.send(dpymd.quote("Some quoted message."))
+	await ctx.send(dpymd.quote_all("Some quoted messages (big text)."))
+
+	# await ctx.send(dpymd.)
+
+
+bot.run("Njk3MDg5NzA1NzUzNDQ0NDUy.XoyOiA.KrqEn4TA3vt7vVNbuV_PMgJw9lA")
